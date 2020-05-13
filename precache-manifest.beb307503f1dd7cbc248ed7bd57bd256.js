c08b882e01c71f76a32fbd8c48b152052b1ca716
pareenk <pareen.karpe@gmail.com>
2020-05-13 09:48:10 -0400

    Updates

---
+++ precache-manifest.beb307503f1dd7cbc248ed7bd57bd256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6febc3c379ce3ff3b14773e0ad8c2873",
+    "url": "/DemoReact/index.html"
+  },
+  {
+    "revision": "79697f91cb0c403ea9ec",
+    "url": "/DemoReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1611a8c2d2c2be80ab3c",
+    "url": "/DemoReact/static/js/2.5b7492d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DemoReact/static/js/2.5b7492d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79697f91cb0c403ea9ec",
+    "url": "/DemoReact/static/js/main.37e67225.chunk.js"
+  },
+  {
+    "revision": "aba036369cdeb0a28d04",
+    "url": "/DemoReact/static/js/runtime-main.8e69df61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DemoReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
